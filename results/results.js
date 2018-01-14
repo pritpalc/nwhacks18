9995b0a982bbe686c5689fdff0148186f8c056da
Pritpal Chauhan <pchauhan@dhcp-128-189-241-162.ubcsecure.wireless.ubc.ca>
2018-01-14 06:56:02 -0800

    Created a table

--- results/results.js
+++ results/results.js
@@ -12,13 +12,20 @@ function initMap() {
 }
 
 function createSideBar() {
+  i = 1;
   for(schedule in schedules) {
     var parent = document.createElement("div");
     parentElement.classList.add("row");
     var childElement = document.createElement("div");
     child.classList.add("col");
-    var text = document.createTextNode("Start: " + START + " End: " + END);
+    var text = document.createTextNode("Option " + i + "Start: " + START + " End: " + END);
     childElement.appendChild(text);
     parentElement.appendChild(child);
+    i++;
+  }
+}
+
+function createCalendar() {
+  for(section in sections) {
   }
 }
